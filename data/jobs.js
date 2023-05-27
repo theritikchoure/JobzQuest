871bf7df1e19da6a311d6c2f3cab60470924b60b
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2023-05-27 08:37:31 +0530

    Added new jobs

--- data/jobs.js
+++ data/jobs.js
@@ -1,8 +1,50 @@
 const jobsData = [
     {
-        role: "Remote Software engineer frontend",
-        company: "Microsoft Corporation",
-        apply_link: "http://msdn.microsoft.com/en-us/library",
-        tags: ["Javascript", "Reactjs", "Vuejs", "Devops", "Python", "C++", "Java"]
-    }
+        role: "Software Developer Job",
+        company: "QuickMove Technologies Pvt. Ltd ",
+        apply_link: "https://cuvette.tech/app/public/job/646f34af6ae5944699a24bc4?referralCode=JT26WF",
+        tags: ["APS.NET", "C#", "Angular"]
+    },
+    {
+        role: "Software Developer",
+        company: "RadiXplore",
+        apply_link: "https://wellfound.com/jobs?job_listing_id=2448164",
+        tags: ["Javascript", "HTML", "CSS", "MongoDB", "ExpressJS", "Nodejs", "VueJs", "AWS", "REST APIs"]
+    },
+    {
+        role: "Backend Developer Job",
+        company: "AgileAxis Privale Limited ",
+        apply_link: "https://cuvette.tech/app/public/job/64704d9782a8d73d22309b50?referralCode=JT26WF",
+        tags: ["Java", "Spring Boot", "SQL"]
+    },
+    {
+        role: "Front-End Engineer",
+        company: "Kisan Network",
+        apply_link: "https://wellfound.com/jobs?job_listing_id=210086",
+        tags: ["Javascript", "HTML", "CSS", "jQuery", "AngularJs", "Nodejs", "Reactjs"]
+    },
+    {
+        role: "Front End Developer",
+        company: "Marquee Equity",
+        apply_link: "https://wellfound.com/jobs?job_listing_id=2659539",
+        tags: ["ReactJS", "RESTful API"]
+    },
+    {
+        role: "Backend Developer (Go Lang)",
+        company: "Afford Medical Technologies",
+        apply_link: "https://wellfound.com/jobs?job_listing_id=2675121",
+        tags: ["Go lang", "Nodejs", "PostgreSQL", "Docker", "Microsoft Azure", "SQL", "Kubernetes", "Nest.Js"]
+    },
+    {
+        role: "Full Stack Developer",
+        company: "FreeStand Sampling",
+        apply_link: "https://wellfound.com/jobs?job_listing_id=1028207",
+        tags: ["Javascript", "HTML", "CSS", "noSQL", "SQL", "Nodejs", "TypeScript", "Firebase", "Reactjs", "TailwindCSS", "tRPC"]
+    },
+    {
+        role: "MERN Stack Developer ( Remote )",
+        company: "AppAvengers",
+        apply_link: "https://wellfound.com/jobs?job_listing_id=1271490",
+        tags: ["Nodejs", "Reactjs", "MERN Stack", "Devops", "Python", "C++", "Java"]
+    },
 ]
