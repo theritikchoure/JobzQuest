a8297a0bf2633a7f4922ec418666f530a92c24fd
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2023-05-28 18:16:15 +0530

    feat: add internship listings

--- data/internships.js
+++ data/internships.js
@@ -7,6 +7,155 @@ const internshipsData = [
     //     tags: ["Skill1", "skill2", "Skill3"]
     // }, 
     {
+        role: 'Frontend Developer Intern',
+        company: 'VILS AIT',
+        tags: ['HTML', 'CSS', 'Git', 'Bootstrap', 'React.js'],
+        apply_link: 'https://wellfound.com/l/2ytg6Y'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Loop',
+        tags: ['Javascript', 'HTML', 'CSS', 'TypeScript', 'React.js'],
+        apply_link: 'https://wellfound.com/l/2yzzk2'
+    },
+    {
+        role: 'Software Developer Intern',
+        company: 'Appleute',
+        tags: ['Node.js', 'React.js'],
+        apply_link: 'https://wellfound.com/l/2ytr7r'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Cynapto Technologies',
+        tags: [
+            'Javascript',
+            'HTML',
+            'CSS',
+            'UI/UX Design',
+            'Bootstrap',
+            'React.js'
+        ],
+        apply_link: 'https://wellfound.com/l/2yBpMZ'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'PNJXN',
+        tags: ['HTML', 'CSS', 'JS', 'WordPress', 'MySQL', 'PHP'],
+        apply_link: 'https://wellfound.com/company/pnjxn/jobs/2638378-frontend-developer-intern?utm_campaign=startup_share&utm_content=startup_share_module&utm_medium=social&utm_term=pnjxn'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'C4Scale',
+        tags: ['Reactjs ', 'WordPress ', 'Git ', 'Gatsby ', 'Bootstrap'],
+        apply_link: 'https://cuvette.tech/app/public/internship/6450c4a2fb6863dda33f74da?referralCode=F71OSK'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Astu Technolgies Pvt Ltd',
+        tags: ['Reactjs ', 'Figma ', 'Firebase ', 'Git.'],
+        apply_link: 'https://cuvette.tech/app/public/internship/644a2aba408307fabf55c07c?referralCode=F71OSK'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Netomi',
+        tags: [],
+        apply_link: 'https://jobs.lever.co/netomi/605c0a33-12eb-4e44-a072-dcdf29e00bd2/apply'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'TriGem',
+        tags: ['Firebase', 'Redux', 'React.js', 'TypeScript'],
+        apply_link: 'https://in.indeed.com/viewjob?jk=e1044550add5c227&from=mobRdr&alid=63a3f49e1ef86228dd5807d2&tk=1h02rrgguttko800&utm_campaign=dt&utm_medium=redir&utm_source=%2Fm%2F&rgtk=1h02rrgguttko800'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Awadh',
+        tags: ['HTML', 'CSS'],
+        apply_link: 'https://in.indeed.com/viewjob?jk=a82c94b6075aca6b&from=mobRdr&alid=640ace71e52aad0b625dee44&tk=1h02umfgduq6h806&utm_campaign=dt&utm_medium=redir&utm_source=%2Fm%2F&rgtk=1h02umfgduq6h806'
+    },
+    {
+        role: 'ReactJS Intern',
+        company: 'BlackSalt Design and Technology',
+        tags: [
+            'ReactJS ',
+            'HTML ',
+            'CSS ',
+            'JavaScript ',
+            'jQuery ',
+            'API integration ',
+            'Git'
+        ],
+        apply_link: 'https://in.indeed.com/m/viewjob?jk=d7988da6a242b788&from=ja&alid=640ace71e52aad0b625dee44&tk=1h05paaacuq6h800&utm_campaign=job_alerts&utm_medium=email&utm_source=jobseeker_emails&rgtk=1h05paaacuq6h800'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Radiant.Digital',
+        tags: ['React | Node JS'],
+        apply_link: 'https://www.indeed.com/cmp/Radiant.digital/jobs?jk=653ef5aed904ea4c&start=0&clearPrefilter=1'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Gro.care',
+        tags: ['JavaScript', 'TypeScript', 'React.JS'],
+        apply_link: 'https://wellfound.com/l/2yRS2y'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Surplus',
+        tags: ['JavaScript', 'React Native'],
+        apply_link: 'https://wellfound.com/l/2yRDFP'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Decision Sciences & Analytics',
+        tags: ['HTML', 'Tailwind CSS', 'React.js', 'Redux'],
+        apply_link: 'https://wellfound.com/l/2yGxrW'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'CollegeStuff',
+        tags: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'React.JS'],
+        apply_link: 'https://wellfound.com/l/2yPsX9'
+    },
+    {
+        role: 'ReactJS Intern',
+        company: 'Co-Rider',
+        tags: ['HTML', 'CSS', 'JavaScript', 'Redux', 'React.JS', 'REST APIs'],
+        apply_link: 'https://wellfound.com/company/corider-1/jobs/2669504-mobile-pwa-react-js-developer-intern'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Cypherock Wallet',
+        tags: [
+            'HTML5',
+            'CSS3',
+            'jQuery',
+            'SCSS',
+            'Bootstrap',
+            'Material',
+            'Tailwind'
+        ],
+        apply_link: 'https://wellfound.com/l/2yy6GG'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Zenskar',
+        tags: ['Javascript', 'HTML', 'CSS', 'React.js'],
+        apply_link: 'https://wellfound.com/l/2yps8E'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'HivePath',
+        tags: ['Vue.js', 'Nuxt.js', 'CSS,HTML', 'React Js and Angular Js'],
+        apply_link: 'https://wellfound.com/company/hivepath/jobs/2521123-frontend-engineer-intern-vue-js'
+    },
+    {
+        role: 'Frontend Developer Intern',
+        company: 'Openfabric',
+        tags: ['Angular'],
+        apply_link: 'https://wellfound.com/l/2yGEpq'
+    },
+    {
         role: "Software Engineering Intern",
         company: "aiXplain",
         apply_link: "https://aixplain.rippling-ats.com/job/617279/software-engineering-intern-ai-backend?s=lif",
